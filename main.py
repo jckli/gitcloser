49e7cc5c33fa3be2ac1cf66e5375eb083353e2a4
lina <me@jackli.dev>
2023-07-23 01:42:17 -0700

    feat (incomplete): just strt algorithm ig

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from src import github
 import aiohttp
 import asyncio
+from src import algorithm
 
 async def main():
-    async with aiohttp.ClientSession() as session:
-        following = await github.get_github_following(session, "jckli")
-        print(following)
+    user_1 = "sindresorhus"
+    user_2 = "kentcdodds"
+    path = await algorithm.bidirectional_bfs(user_1, user_2)
+    print(path)
 
 if __name__ == "__main__":
     asyncio.run(main())
